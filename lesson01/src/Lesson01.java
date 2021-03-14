6ff287b35ffc4f4bdd50661624b7ed235d6070db
RICARDO-PC\paula <paulakaszczyszyn@gmail.com>
2021-03-14 21:48:25 +0100

    reading age

--- lesson01/src/Lesson01.java
+++ lesson01/src/Lesson01.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Lesson01 {
     public static void main(String[] args) {
-        System.out.println("Paula to diva");
+        System.out.println("Podaj swoj wiek: ");
+        int wiek = new Scanner(System.in).nextInt();
+        System.out.println("Twój wiek to: " + wiek);
     }
 }
