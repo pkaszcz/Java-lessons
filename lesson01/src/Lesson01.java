5b3fd016f64455c1ee4d536738422b67f2cf5ab5
RICARDO-PC\paula <paulakaszczyszyn@gmail.com>
2021-03-14 22:26:46 +0100

    simplifcation of the code

--- lesson01/src/Lesson01.java
+++ lesson01/src/Lesson01.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 
+// równy: ==
+// mniejszy lub równy: >=
+// i: &&
+// podwójny nawias gdy wpisujemy dwie dane
+
 public class Lesson01 {
     public static void main(String[] args) {
         System.out.println("Podaj swoj wiek: ");
         int wiek = new Scanner(System.in).nextInt();
         System.out.println("Twój wiek to: " + wiek);
 
-        // równy: ==
-        // mniejszy lub równy: >=
-        // i: &&
+        if (wiek < 0) return;
+        if (wiek > 100) return;
 
-        // podwójny nawias gdy wpisujemy dwie dane
-
-        if ((wiek > 0) && (wiek < 20)) {
+        if (wiek < 20) {
             System.out.println("Jesteś młody");
         } else if (wiek >= 20) {
             System.out.println("Jesteś w średnim wieku lub stary");
-        } else {
-            System.out.println("Wprowadziłeś zły wiek");
         }
 
     }
