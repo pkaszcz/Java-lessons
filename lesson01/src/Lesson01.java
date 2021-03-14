9d99c523f5f6ff8c2cd0ef93f1ef3faf48df2967
RICARDO-PC\paula <paulakaszczyszyn@gmail.com>
2021-03-14 22:16:36 +0100

    assess person age

--- lesson01/src/Lesson01.java
+++ lesson01/src/Lesson01.java
@@ -5,5 +5,20 @@ public class Lesson01 {
         System.out.println("Podaj swoj wiek: ");
         int wiek = new Scanner(System.in).nextInt();
         System.out.println("Twój wiek to: " + wiek);
+
+        // równy: ==
+        // mniejszy lub równy: >=
+        // i: &&
+
+        // podwójny nawias gdy wpisujemy dwie dane
+
+        if ((wiek > 0) && (wiek < 20)) {
+            System.out.println("Jesteś młody");
+        } else if (wiek >= 20) {
+            System.out.println("Jesteś w średnim wieku lub stary");
+        } else {
+            System.out.println("Wprowadziłeś zły wiek");
+        }
+
     }
 }
