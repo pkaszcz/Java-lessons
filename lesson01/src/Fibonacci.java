69a723d9be77b50e5a87888d490a7745e0c3e982
RICARDO-PC\paula <paulakaszczyszyn@gmail.com>
2021-04-09 21:20:33 +0200

    Fibonacci

--- lesson01/src/Fibonacci.java
+++ lesson01/src/Fibonacci.java
@@ -2,16 +2,26 @@ import java.util.Scanner;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        int f = new Scanner(System.in).nextInt();
-        int x = 0;
-        int y = 1;
+        int nMax = new Scanner(System.in).nextInt();
+        int n2 = 0; //n - 2
+        int n1 = 1; //n - 1
+        int f = 1928; //wartość F(n)
 
-        if (f > 19) return;
+        if (nMax < 0) return;
+        if (nMax > 20) return;
 
-        if (f == 0) System.out.println(0);
-        if (f == 1) System.out.println(1);
+        if (nMax == 0) System.out.print("0");
+        if (nMax >= 1) System.out.print("0 1 ");
 
+        //0 1 1 2 3 5 8 13
 
+        for (int n = 2; n < nMax; n++) {
+            f = n1 + n2;
+            System.out.print(f + " ");
+            n2 = n1;
+            n1 = f;
 
+        }
+        System.out.println();
     }
 }
