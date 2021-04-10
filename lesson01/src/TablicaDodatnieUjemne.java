ab413998a4c8ab37097b99f9842b2ef75559f036
RICARDO-PC\paula <paulakaszczyszyn@gmail.com>
2021-04-10 21:15:43 +0200

    TablicaKwadrat

--- lesson01/src/TablicaDodatnieUjemne.java
+++ lesson01/src/TablicaDodatnieUjemne.java
@@ -1,20 +1,23 @@
-public class TablicaKwadrat {
+import java.util.Scanner;
+
+public class TablicaDodatnieUjemne {
     public static void main(String[] args) {
-        int[] tab = new int[20];
+        int[] tab = new int[new Scanner(System.in).nextInt()];
         int len = tab.length;
         int x = 1;
         for (int i = 0; i < len; i++) {
-
+            //tablica:{1, -2, 3, -4, 5, -6, ...}
             tab[i] = x;
-            x += 1;
+            x += 2;
         }
 
         System.out.print("tablica:{");
 
         for (int i = 0; i < len; i++) {
-            System.out.print(tab[i] * tab[i]);
+            System.out.print(tab[i]);
             if (i < len - 1) System.out.print(",");
         }
         System.out.print("}");
+
     }
 }
