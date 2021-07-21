cfd621369724a9c7c5e613fc8a74914397d98662
RICARDO-PC\paula <paulakaszczyszyn@gmail.com>
2021-07-21 18:51:55 +0200

    CheckBubbleSort

--- lesson01/src/CheckBubbleSort.java
+++ lesson01/src/CheckBubbleSort.java
@@ -1,17 +1,20 @@
 import java.util.Random;
 
 public class CheckBubbleSort {
-    public static void main(String[] args) {
-        int[] tab = new int[20];
-        int len = tab.length;
-        int indexHead = 0;
-        int indexI = 0;
 
-        for (int i = 0; i < len; i++) {
+    static int[] generateRandomArray() {
+        int[] tab = new int[20];
+        for (int i = 0; i < tab.length; i++) {
             tab[i] = new Random().nextInt(100) + 1;
             System.out.print(tab[i] + " ");
         }
-        System.out.println();
+        return tab;
+    }
+
+    static int[] sort(int[] tab) {
+        int len = tab.length;
+        int indexHead = 0;
+        int indexI = 0;
 
         for (int j = 0; j < len - 1; j++) {
             for (int i = indexHead; i < len - 1; i++) {
@@ -22,19 +25,32 @@ public class CheckBubbleSort {
                 }
             }
         }
-        for (int i = 0; i < len; i++) {
-            System.out.print(tab[i] + " ");
-        }
+        return tab;
+    }
 
-        System.out.println("");
-
+    static boolean isSorted(int[] tab) {
         boolean isSorted = true;
-        for (int i = 0; i < len - 1; i++) {
+        for (int i = 0; i < tab.length - 1; i++) {
             if (tab[i] > tab[i + 1]) {
                 isSorted = false;
                 break;
             }
         }
-        System.out.println("is sorted = " + isSorted);
+        return isSorted;
+    }
+
+    static void show(int[] tab) {
+        for (int i = 0; i < tab.length; i++) {
+            System.out.print(tab[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] tab = generateRandomArray();
+        System.out.println();
+        tab = sort(tab);
+        show(tab);
+        System.out.println("");
+        System.out.println("is sorted = " + isSorted(tab));
     }
 }
